be700cb07a0582ead1e3f16b0905c8a5249f60c6
KKonsur <87540841+KKonsur@users.noreply.github.com>
2022-05-03 15:56:05 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+const ratingScore = document.querySelectorAll('.ratingBox__ratingScore')
+const ratingScoreX = document.querySelector('.ratingBox__ratingScore--active')
+const submitScore = document.querySelector('.ratingBox__submitButton')
+let resultValue
+let isChecked = false
+
+ratingScore.forEach(element => {
+    element.addEventListener('click', e => {
+        isChecked = !isChecked
+        ratingScore.forEach(element => element.classList.remove('ratingBox__ratingScore--active'))
+        element.classList.add('ratingBox__ratingScore--active')
+        resultValue = e.target.textContent
+    })
+})
+
+submitScore.addEventListener('click', () => {
+    if (isChecked) {
+        document.querySelector('.thankYouState').classList.add('thankYouState--active')
+        document.querySelector('.ratingBox').remove()
+        document.querySelector('.thankYouState__resultValue').textContent = resultValue
+    } else {
+        alert('Please rate us :)')
+    }
+})
+
+
